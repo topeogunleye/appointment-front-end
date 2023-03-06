28b5cfbeae5bd3a07e8444fdbe3885eae91e64d6
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-06 17:16:17 +0100

    Add sticky to navbar and absolute position to social media icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,7 +6,7 @@ export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="grid align-items-center w-64 h-screen px-4 py-8 bg-white border-2 border-rose-600">
+    <div className="grid align-items-center w-64 h-screen px-4 py-8 bg-white border-2 border-rose-600 sticky">
     <div className="grid align-items-center">
       <img src={Image} alt="logo" className="" />
       <h1 className="mt-2 text-white font-bold text-lg">Company Name</h1>
@@ -37,7 +37,7 @@ export default function Sidebar() {
       REMOVE CAR REPAIR
     </a>
 
-      <div className="social-media flex justify-between w-4/5">
+      <div className="social-media flex justify-between w-3/5 absolute bottom-2 left-3">
         <a
           href="https://github.com/topeogunleye
               "
