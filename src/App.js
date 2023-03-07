bd4479f04e721dc4aca4d2321169b4864bbb029c
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-07 09:01:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,47 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
+import NewReserveForm from './components/pages/newreserveform/NewReserveForm';
+import RemoveReservationForm from './components/pages/removereservationForm/RemoveReservationForm';
+import AddReservationForm from './components/pages/addreservationform/AddReservationForm';
+import Services from './components/pages/services/Services';
+import Reservations from './components/pages/reservations/Reservations';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={(
-              <div className="flex">
-                <Sidebar />
-                <div>This is the main index page for all of the services</div>
-              </div>
-            )}
-          />
-          <Route
-            path="/NewReserveForm"
-            element={(
-              <>
-                <div>This is the add new reservation form page</div>
-              </>
-            )}
-          />
-          <Route
-            path="/services"
-            element={(
-              <div className="flex">
-                <Sidebar />
-                <div>This will show the details of each service</div>
-              </div>
-            )}
-          />
-          <Route
-            path="/reservations"
-            element={(
-              <>
+          <Route path="/" element={<Sidebar />}>
+            <Route
+              path="/"
+              element={(
                 <div className="flex">
                   <Sidebar />
-                  <div>
-                    This is the page that shows all the reservations you have
-                    created as a user
-                  </div>
+                  <div>This is the main index page for all of the services</div>
                 </div>
-              </>
-            )}
-          />
-          <Route
-            path="/AddReservationForm"
-            element={(
-              <>
-                <div>This is the form for adding a new car repair service</div>
-              </>
-            )}
-          />
-          <Route
-            path="/RemoveervationForm"
-            element={(
-              <>
-                <div>
-                  This is the page where we can remove a car repair service
-                </div>
-              </>
-            )}
-          />
+              )}
+            />
+            <Route
+              path="/NewReserveForm"
+              element={<NewReserveForm />}
+            />
+            <Route
+              path="/services"
+              element={<Services />}
+            />
+            <Route
+              path="/reservations"
+              element={<Reservations />}
+            />
+            <Route
+              path="/AddReservationForm"
+              element={<AddReservationForm />}
+            />
+            <Route
+              path="/RemoveReservationForm"
+              element={<RemoveReservationForm />}
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
