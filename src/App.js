82024c73adf267372b186a0d4f6578eac16573b1
David  Amon-Cofie <david@goyazi.com>
2023-03-07 01:11:46 +0000

    Add route paths for each NavLink

--- src/App.js
+++ src/App.js
@@ -6,7 +6,64 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Sidebar />} />
+          <Route
+            path="/"
+            element={
+              <div className="flex">
+                <Sidebar />
+                <div>This is the main index page for all of the services</div>
+              </div>
+            }
+          />
+          <Route
+            path="/reservenewform"
+            element={
+              <>
+                <div>This is the add new reservation form page</div>
+              </>
+            }
+          />
+          <Route
+            path="/services"
+            element={
+              <div className="flex">
+                <Sidebar />
+                <div>This will show the details of each service</div>
+              </div>
+            }
+          />
+          <Route
+            path="/reservations"
+            element={
+              <>
+                <div className="flex">
+                  <Sidebar />
+                  <div>
+                    This is the page that shows all the reservations you have
+                    created as a user
+                  </div>
+                </div>
+              </>
+            }
+          />
+          <Route
+            path="/addcarrepairform"
+            element={
+              <>
+                <div>This is the form for adding a new car repair service</div>
+              </>
+            }
+          />
+          <Route
+            path="/removecarrepairform"
+            element={
+              <>
+                <div>
+                  This is the page where we can remove a car repair service
+                </div>
+              </>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
