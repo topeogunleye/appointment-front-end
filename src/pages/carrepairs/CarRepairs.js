f20b26cbe189aa35547849cb54dbddb82c52fb0f
David  Amon-Cofie <david@goyazi.com>
2023-03-08 03:09:02 +0000

    Configure CarRepair.js component to consume store

--- src/pages/carrepairs/CarRepairs.js
+++ src/pages/carrepairs/CarRepairs.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './CarRepair.css';
+import { useSelector, useDispatch } from 'react-redux';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import { fetchServices } from '../../redux/carRepairServices';
 
-const CarRepairs = () => (
-  <div className="flex">
-    <Sidebar />
-    <h1 className="m-auto p-32">This is the add new reservation form page</h1>
-  </div>
-);
+const CarRepairs = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchServices());
+  }, []);
+
+  const services = useSelector((state) => state.services);
+  services.isLoading ? console.log('loading ') : console.log(services.services);
+  return (
+    <div className="flex">
+      <Sidebar />
+      <h1 className="m-auto p-32">This is the add new reservation form page</h1>
+    </div>
+  );
+};
 
 export default CarRepairs;
