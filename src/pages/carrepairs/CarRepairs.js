885a161e986825a3747c9d0edac291ec2126a704
David  Amon-Cofie <david@goyazi.com>
2023-03-09 20:53:22 +0000

    Add fuctionality to display 3 services in carousel

--- src/pages/carrepairs/CarRepairs.js
+++ src/pages/carrepairs/CarRepairs.js
@@ -3,6 +3,7 @@ import './CarRepair.css';
 import { useSelector, useDispatch } from 'react-redux';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { fetchServices } from '../../redux/carRepairServices';
+import ServiceComponent from '../../components/ServiceComponent';
 
 const CarRepairs = () => {
   const dispatch = useDispatch();
@@ -16,8 +17,7 @@ const CarRepairs = () => {
 
   const [index, setIndex] = useState(0);
   // const length = 500;
-  const { length } = services.services;
-  console.log(length);
+  const length = services.services.length;
 
   const handlePrevious = () => {
     const newIndex = index - 1;
@@ -28,6 +28,30 @@ const CarRepairs = () => {
     const newIndex = index + 1;
     setIndex(newIndex >= length ? 0 : newIndex);
   };
+
+  const current_carousel = [];
+  if (!services.isLoading) {
+    const carousel_first = services.services[index];
+    let carousel_second;
+    let carousel_third;
+
+    if (index == length - 1) {
+      carousel_second = services.services[0];
+      carousel_third = services.services[1];
+    } else if (index + 1 == length - 1) {
+      carousel_second = services.services[index + 1];
+      carousel_third = services.services[0];
+    } else {
+      carousel_second = services.services[index + 1];
+      carousel_third = services.services[index + 2];
+    }
+
+    current_carousel.push(carousel_first);
+    current_carousel.push(carousel_second);
+    current_carousel.push(carousel_third);
+
+    console.log(current_carousel);
+  }
 
   return (
     <div className="flex">
@@ -48,7 +72,11 @@ const CarRepairs = () => {
             >
               {'<'}
             </button>
-            <p>{index}</p>
+            <div>
+              {current_carousel.map((service) => (
+                <ServiceComponent key={service.id} service={service} />
+              ))}
+            </div>
             <button
               type="button"
               className="border-2 border-black p-2 rounded-l-full w-20"
