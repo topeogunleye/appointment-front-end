f9ba7910889b45de27e58a8e1f94538ae4ce4227
David  Amon-Cofie <david@goyazi.com>
2023-03-09 10:22:00 +0000

    Consume data from placeholder API

--- src/pages/carrepairs/CarRepairs.js
+++ src/pages/carrepairs/CarRepairs.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CarRepair.css';
 import { useSelector, useDispatch } from 'react-redux';
 import Sidebar from '../../components/Sidebar/Sidebar';
@@ -12,11 +12,53 @@ const CarRepairs = () => {
   }, []);
 
   const services = useSelector((state) => state.services);
-  services.isLoading ? console.log('loading ') : console.log(services.services);
+  console.log(services.services);
+
+  const [index, setIndex] = useState(0);
+  // const length = 500;
+  const { length } = services.services;
+  console.log(length);
+
+  const handlePrevious = () => {
+    const newIndex = index - 1;
+    setIndex(newIndex < 0 ? length - 1 : newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = index + 1;
+    setIndex(newIndex >= length ? 0 : newIndex);
+  };
+
   return (
     <div className="flex">
       <Sidebar />
-      <h1 className="m-auto p-32">This is the add new reservation form page</h1>
+      <div className="border-2 border-black m-auto">
+        <div className="w-full">
+          <div>
+            <h1>Services we render</h1>
+            <p>Please choose the service you require</p>
+            <br />
+            <p>....................</p>
+          </div>
+          <div className="flex justify-between">
+            <button
+              type="button"
+              className="border-2 border-black p-2 rounded-r-full w-20"
+              onClick={handlePrevious}
+            >
+              {'<'}
+            </button>
+            <p>{index}</p>
+            <button
+              type="button"
+              className="border-2 border-black p-2 rounded-l-full w-20"
+              onClick={handleNext}
+            >
+              {'>'}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
