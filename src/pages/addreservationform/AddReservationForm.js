5c4a6aff287675db626f99fb60c8ac991b16ee21
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-09 11:53:40 +0100

    Add ReservationForm Index page and css

--- src/pages/addreservationform/AddReservationForm.js
+++ src/pages/addreservationform/AddReservationForm.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import './AddReservationForm.css';
 
 function AddReservationForm() {
   return (
-    <div className="flex">
+    <div className="grid grid-cols-custom">
       <Sidebar />
-      <h2 className="m-auto p-32">This is the form for adding a new car repair service</h2>
+      <div className="grid items-center min-h-screen bg-cover bg-repeat bg-top text-white">
+        <div className="h-80 grid items-center">
+          <h2 className="leading-10 tracking-widest underline underline-offset-8 decoration-solid font-bold">BOOK A CAR REPAIR TODAY</h2>
+          <p className="w-3/4 m-auto leading-7 text-sm">
+            There are 34 different repair services at Barefoot Mechanic.
+            Todays five shops are in production:
+            the manual transmission PX and the modern CVT transmission S, LX, GT, and GT5.
+            We have repair shops all over the globe which some include a free first month
+            subscription
+            If you wish to find out if a free repair is available in your area.
+            please use the selector below
+          </p>
+          <div className="flex items-center justify-between m-auto w-80">
+            <Link
+              to="/CarRepairForm/"
+              className="button bg-green flex justify-center items-center justify-between w-36"
+            >
+              London
+              <FaIcons.FaArrowDown className="inline" />
+            </Link>
+
+            <Link
+              to="/ServiceForm/"
+              type="button"
+              className="button bg-white text-green text-center justify-between w-36"
+            >
+              Book Now
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
