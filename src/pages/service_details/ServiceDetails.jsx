97980a00eea7c9b4171f588cdd0888688b91dd89
David  Amon-Cofie <david@goyazi.com>
2023-03-09 18:54:28 +0000

    Create service details page

---
+++ src/pages/service_details/ServiceDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ServiceDetails() {
+  return (
+    <div>
+      <div>
+        <div>This will hold the image of the service</div>
+        <button>{'<'}</button>
+      </div>
+      <div>
+        <h1>Name</h1>
+        <div>This will hold the table of details</div>
+        <button type="button">Book Reservation</button>
+      </div>
+    </div>
+  );
+}
+
+export default ServiceDetails;
