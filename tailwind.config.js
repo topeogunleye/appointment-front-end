db96ebdb1bb390ddfcce549d346c790809b40ba0
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-09 12:06:31 +0100

    Add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./images/hero2.jpg')",
+      },
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
+      gridTemplateColumns: {
+        custom: 'minmax(150px, 18%) 1fr',
+      },
+      letterSpacing: {
+        widest: '.5em',
+      },
+      colors: {
+        green: '#90ba04',
+      },
+    },
   },
   plugins: [],
 };
