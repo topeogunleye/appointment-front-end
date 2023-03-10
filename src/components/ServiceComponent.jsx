bc85846d17a334674c76d5eee85ae87683b69147
David  Amon-Cofie <david@goyazi.com>
2023-03-10 00:05:53 +0000

    Add navigation to service details page on select

--- src/components/ServiceComponent.jsx
+++ src/components/ServiceComponent.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ServiceComponent = (props) => {
+  const navigate = useNavigate();
+
+  const { focusedService } = props;
   const { name, email, body } = props.service;
   console.log(props);
   return (
-    <div>
+    <div
+      onClick={() => {
+        navigate('/service_details');
+        focusedService(props.service);
+      }}
+    >
       <p>{name}</p>
       <p>{email}</p>
       <p>{body}</p>
