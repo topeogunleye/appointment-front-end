9484e3d327fdb2afb1dc5abb96c7716d2f954e92
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-10 10:24:55 +0100

    Add styles for SignUpLogin component

--- src/pages/signuplogin/SignUpLogin.js
+++ src/pages/signuplogin/SignUpLogin.js
@@ -54,15 +54,21 @@ const Loginsignup = () => {
   };
 
   return (
-    <div>
-      <button type="button" className="ui button" onClick={() => handleFormSwitch('signUp')}>Sign Up</button>
-      <button type="button" className="ui button" onClick={() => handleFormSwitch('login')}>Log In</button>
-      {
-        renderForm()
-      }
-      <button type="button" onClick={handleAuthClick} className="ui button">Access Authorized Route</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full">
+        <div className="text-center text-2xl font-bold mb-8">
+          <h1 className="text-gray-900">Welcome!</h1>
+        </div>
+        <div className="flex space-x-4 mb-4">
+          <button type="button" className="bg-gray-900 text-white py-2 px-4 rounded" onClick={() => handleFormSwitch('signUp')}>Sign Up</button>
+          <button type="button" className="bg-gray-200 text-gray-900 py-2 px-4 rounded" onClick={() => handleFormSwitch('login')}>Log In</button>
+        </div>
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          {renderForm()}
+        </div>
+        <button type="button" onClick={handleAuthClick} className="bg-gray-900 text-white py-2 px-4 rounded">Access Authorized Route</button>
+      </div>
     </div>
-
   );
 };
 
