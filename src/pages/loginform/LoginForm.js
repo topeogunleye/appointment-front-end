f0dc60c06cb005ae0fe8481995dd06fa91f3d621
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-10 10:25:29 +0100

    Add LoginForm component styles

--- src/pages/loginform/LoginForm.js
+++ src/pages/loginform/LoginForm.js
@@ -34,26 +34,36 @@ function LoginForm(props) {
     setUsername('');
     setPassword('');
   };
-  const formDivStyle = {
-    margin: 'auto',
-    padding: '20px',
-    width: '80%',
-  };
+
   return (
-    <div>
-      <div style={formDivStyle}>
-        <h1>Log In</h1>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-2/3 bg-gray-100 rounded-lg p-8">
+        <h1 className="text-2xl mb-4">Log In</h1>
         <form className="ui form" onSubmit={handleSubmit}>
-          <div className="field">
-            <label>Username</label>
-            <input value={username} onChange={handleUsernameChange} type="text" placeholder="username" />
+          <div className="mb-4">
+            <label htmlFor="username" className="block font-bold mb-2">Username</label>
+            <input 
+              id="username"
+              value={username} 
+              onChange={handleUsernameChange} 
+              type="text" 
+              placeholder="username"
+              className="block appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            />
           </div>
-          <div className="field">
-            <label>Password</label>
-            <input value={password} onChange={handlePasswordChange} type="password" placeholder="password" />
+          <div className="mb-4">
+            <label htmlFor="password" className="block font-bold mb-2">Password</label>
+            <input 
+              id="password"
+              value={password} 
+              onChange={handlePasswordChange} 
+              type="password" 
+              placeholder="password"
+              className="block appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            />
           </div>
 
-          <button className="ui button" type="submit">Submit</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
         </form>
       </div>
     </div>
