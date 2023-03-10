681b4edc328e963d18187ab906b8f6c2ab7795e2
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-10 10:04:28 +0100

    Add sign in page

---
+++ src/pages/signuplogin/SignUpLogin.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import LoginForm from '../loginform/LoginForm';
+import SignInForm from '../signinform/SignInForm';
+
+const Loginsignup = () => {
+  const [user, setUser] = useState({});
+  const [form, setForm] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      fetch('http://localhost:3000/auto_login', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setUser(data);
+        // console.log(data)
+        });
+    }
+  }, []);
+
+  const handleLogin = (user) => {
+    setUser(user);
+  };
+
+  const handleFormSwitch = (input) => {
+    setForm(input);
+  };
+
+  const handleAuthClick = () => {
+    const token = localStorage.getItem('token');
+    fetch('http://localhost:3000/user_is_authed', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));
+  };
+
+  console.log(user);
+
+  const renderForm = () => {
+    switch (form) {
+      case 'login':
+        return <LoginForm handleLogin={handleLogin} />;
+        break;
+      default:
+        return <SignInForm handleLogin={handleLogin} />;
+    }
+  };
+
+  return (
+    <div>
+      <button type="button" className="ui button" onClick={() => handleFormSwitch('signUp')}>Sign Up</button>
+      <button type="button" className="ui button" onClick={() => handleFormSwitch('login')}>Log In</button>
+      {
+        renderForm()
+      }
+      <button type="button" onClick={handleAuthClick} className="ui button">Access Authorized Route</button>
+    </div>
+
+  );
+};
+
+export default Loginsignup;
