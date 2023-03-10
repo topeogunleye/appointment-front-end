868e8d26f5fab994b1bd9693bc77fd9bee4632d6
David  Amon-Cofie <david@goyazi.com>
2023-03-10 00:08:18 +0000

    Display selected service on details page

--- src/pages/service_details/ServiceDetails.jsx
+++ src/pages/service_details/ServiceDetails.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-function ServiceDetails() {
+function ServiceDetails(props) {
+  console.log(props);
+
+  const { name, email, body } = props.details;
   return (
     <div className="border-2 border-black w-1/2 mx-auto">
       <div>
-        <div>This will hold the image of the service</div>
+        <div>
+          This will hold the image of the service but for now lets render email:{' '}
+          {email}
+        </div>
         <button>{'<'}</button>
       </div>
       <div>
-        <h1>Name</h1>
-        <div>This will hold the table of details</div>
+        <h1>{name}</h1>
+        <div>{body}</div>
         <button type="button">Book Reservation</button>
       </div>
     </div>
