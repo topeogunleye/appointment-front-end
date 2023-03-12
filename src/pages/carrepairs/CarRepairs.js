c84f3aeafd0c4c19386936a36620aa3481cdda9e
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-12 21:29:53 +0100

    Add styles to CarRepairs

--- src/pages/carrepairs/CarRepairs.js
+++ src/pages/carrepairs/CarRepairs.js
@@ -1,15 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import './CarRepair.css';
 import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import ServiceComponent from '../../components/ServiceComponent';
 import { fetchServices } from '../../redux/carRepairServices';
+import Carousel from '../../components/Carousel/Carousel';
 
 const CarRepairs = () => {
   const dispatch = useDispatch();
 
+  const images = [
+    {
+      id: 1,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 1',
+      text: 'Image 1',
+      title: 'Repair 1',
+      description: 'This is the description for image 1',
+    },
+    {
+      id: 2,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 2',
+      text: 'Image 2',
+      title: 'Repair 2',
+      description: 'This is the description for image 2',
+    },
+    {
+      id: 3,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 3',
+      text: 'Image 3',
+      title: 'Repair 3',
+      description: 'This is the description for image 3',
+    },
+    {
+      id: 4,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 4',
+      text: 'Image 4',
+      title: 'Repair 4',
+      description: 'This is the description for image 4',
+    },
+    {
+      id: 5,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 5',
+      text: 'Image 5',
+      title: 'Repair 5',
+      description: 'This is the description for image 5',
+    },
+    {
+      id: 6,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 6',
+      text: 'Image 6',
+      title: 'Repair 6',
+      description: 'This is the description for image 6',
+    },
+    {
+      id: 7,
+      src: 'https://picsum.photos/200/200',
+      alt: 'Image 7',
+      text: 'Image 7',
+      title: 'Repair 7',
+      description: 'This is the description for image 7',
+    },
+  ];
+
+  const { handleServiceClick } = props;
+
+  const focusedService = (data) => {
+    handleServiceClick(data);
+  };
+
   useEffect(() => {
     dispatch(fetchServices());
-  }, []);
+  }, [dispatch]);
 
   const services = useSelector((state) => state.services);
   console.log(services.services);
@@ -17,7 +85,6 @@ const CarRepairs = () => {
   const [index, setIndex] = useState(0);
   // const length = 500;
   const { length } = services.services;
-  console.log(length);
 
   const handlePrevious = () => {
     const newIndex = index - 1;
@@ -29,35 +96,45 @@ const CarRepairs = () => {
     setIndex(newIndex >= length ? 0 : newIndex);
   };
 
+  const currentCarousel = [];
+  if (!services.isLoading) {
+    const carouselFirst = services.services[index];
+    let carouselSecond;
+    let carouselThird;
+
+    if (index === length - 1) {
+      [carouselSecond, carouselThird] = [
+        services.services[0],
+        services.services[1],
+      ];
+    } else if (index + 1 === length - 1) {
+      [carouselSecond, carouselThird] = [
+        services.services[index + 1],
+        services.services[0],
+      ];
+    } else {
+      [carouselSecond, carouselThird] = [
+        services.services[index + 1],
+        services.services[index + 2],
+      ];
+    }
+
+    currentCarousel.push(carouselFirst);
+    currentCarousel.push(carouselSecond);
+    currentCarousel.push(carouselThird);
+
+    console.log(currentCarousel);
+  }
+
   return (
-    <div className="flex">
+    <div className="grid grid-cols-custom">
       <Sidebar />
-      <div className="border-2 border-black m-auto">
-        <div className="w-full">
-          <div>
-            <h1>Services we render</h1>
-            <p>Please choose the service you require</p>
-            <br />
-            <p>....................</p>
-          </div>
-          <div className="flex justify-between">
-            <button
-              type="button"
-              className="border-2 border-black p-2 rounded-r-full w-20"
-              onClick={handlePrevious}
-            >
-              {'<'}
-            </button>
-            <p>{index}</p>
-            <button
-              type="button"
-              className="border-2 border-black p-2 rounded-l-full w-20"
-              onClick={handleNext}
-            >
-              {'>'}
-            </button>
-          </div>
+      <div className="grid items-center min-h-screen font-bold">
+        <div className="grid place-items-center justify-center">
+          <h1 className="text-black-600 text-2xl tracking-wider">LATEST SERVICES</h1>
+          <p className="text-sm text-gray-400">Please select a new Service</p>
         </div>
+        <Carousel images={images} />
       </div>
     </div>
   );
