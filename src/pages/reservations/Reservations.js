27d869d1d6fb6d4384c904802cccd0114bc9565d
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-14 12:44:01 +0100

    Add sidebar to reservations and display current user reservation only

--- src/pages/reservations/Reservations.js
+++ src/pages/reservations/Reservations.js
@@ -1,89 +1,99 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import { fetchServices } from '../../redux/reservationSlice';
 
 const Reservations = () => {
   const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservation.formData);
+  console.log(reservations);
   const isLoading = useSelector((state) => state.isLoading);
-
+  const user = useSelector((state) => state.auth.user); // assuming you have a currentUser state
+  console.log(user);
   useEffect(() => {
     dispatch(fetchServices());
   }, [dispatch]);
 
+  const userReservations = reservations.filter(
+    (reservation) => reservation.id === user.id,
+  );
+
   return (
-    <div className="container mx-auto my-4">
-      <h1 className="text-3xl font-bold mb-4">Reservations</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Vehicle
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Model
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Year
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Color
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Location
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Service
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {reservations.map((service) => (
-              <tr key={service.id}>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                  {service.vehicle}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {service.model}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {service.year}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {service.color}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {service.location}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {service.service}
-                </td>
+    <div className="grid grid-cols-custom">
+      <Sidebar />
+      <div className="container mx-auto my-4">
+        <h1 className="text-3xl font-bold mb-4">Reservations</h1>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Vehicle
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Model
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Year
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Color
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Location
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Service
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {userReservations.map((service) => (
+                <tr key={service.id}>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    {service.vehicle}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {service.model}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {service.year}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {service.color}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {service.location}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {service.service}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 };
