ae545d9196d96dac3ec05ea21c8ea564e22159a0
amon-cofie <ddd.amon123@gmail.com>
2023-03-16 13:30:30 +0000

    Remove redundant imports in AddReservationForm.js

--- src/pages/addreservationform/AddReservationForm.js
+++ src/pages/addreservationform/AddReservationForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as FaIcons from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -7,7 +7,9 @@ import './AddReservationForm.css';
 import { setSelectedOption } from '../../redux/selectDropdownSlice';
 
 function AddReservationForm() {
-  const selectedOption = useSelector((state) => state.selectDropdown.selectedOption);
+  const selectedOption = useSelector(
+    (state) => state.selectDropdown.selectedOption,
+  );
   const dispatch = useDispatch();
 
   const handleChange = (event) => {
@@ -19,19 +21,24 @@ function AddReservationForm() {
       <Sidebar />
       <div className="grid items-center min-h-screen bg-cover bg-repeat bg-top text-white">
         <div className="h-80 grid items-center">
-          <h2 className="leading-10 tracking-widest underline underline-offset-8 decoration-solid font-bold">BOOK A CAR REPAIR TODAY</h2>
+          <h2 className="leading-10 tracking-widest underline underline-offset-8 decoration-solid font-bold">
+            BOOK A CAR REPAIR TODAY
+          </h2>
           <p className="w-3/4 m-auto leading-7 text-sm">
-            There are 34 different repair services at Barefoot Mechanic.
-            Todays five shops are in production:
-            the manual transmission PX and the modern CVT transmission S, LX, GT, and GT5.
-            We have repair shops all over the globe which some include a free first month
-            subscription
-            If you wish to find out if a free repair is available in your area.
+            There are 34 different repair services at Barefoot Mechanic. Todays
+            five shops are in production: the manual transmission PX and the
+            modern CVT transmission S, LX, GT, and GT5. We have repair shops all
+            over the globe which some include a free first month subscription If
+            you wish to find out if a free repair is available in your area.
             please use the selector below
           </p>
           <div className="flex items-center justify-between m-auto w-80">
             <div className="relative">
-              <select value={selectedOption} onChange={handleChange} className="block appearance-none w-full bg-green border border-white-300 hover:border-white-400 rounded-full py-2 pl-4 pr-8 leading-tight focus:outline-none focus:ring focus:border-blue-300">
+              <select
+                value={selectedOption}
+                onChange={handleChange}
+                className="block appearance-none w-full bg-green border border-white-300 hover:border-white-400 rounded-full py-2 pl-4 pr-8 leading-tight focus:outline-none focus:ring focus:border-blue-300"
+              >
                 <option>New York</option>
                 <option>Los Angeles</option>
                 <option>Chicago</option>
