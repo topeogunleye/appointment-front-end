745faa0f612a97a92648cadfe03854f11f9a4749
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-17 15:03:06 +0100

    use const instead of function

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 
-function Services() {
-  return (
-    <div className="flex">
-      <Sidebar />
-      <h2 className="m-auto p-32">This will show the details of each service</h2>
-    </div>
-  );
-}
+const Services = () => (
+  <div className="flex">
+    <Sidebar />
+    <h2 className="m-auto p-32">This will show the details of each service</h2>
+  </div>
+);
 
 export default Services;
